68c5d09351513c49197c821685808c8dc0a3c300
Abhishek Poddar <98929252+abhishekpoddar29@users.noreply.github.com>
2024-09-11 23:44:57 +0530

    Add files via upload

--- Array/remove_duplicate.py
+++ Array/remove_duplicate.py
@@ -1,3 +1,16 @@
+'''
+Given an integer array arr sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in arr.
+Consider the number of unique elements of arr to be k, to get accepted, you need to do the following things:
+Change the array arr such that the first k elements of arr contain the unique elements in the order they were present in arr initially. The remaining elements of arr are not important as well as the size of arr.
+Return k.
+
+Example 1:
+Input: arr = [1,1,2]
+Output:arr = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of arr being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+'''
+
 def remove_dup(arr):
     if not arr:
         return []
